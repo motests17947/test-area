2d41fe3966e6725a70523f297b4e4025a077a5ab
sheng <mico@ipopos.com>
2024-05-09 10:52:19 +0800

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // base: './',
-  base: '/test-area/tree/master/',
+  base: process.env.NODE_ENV === 'production' ? '/test-area/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
